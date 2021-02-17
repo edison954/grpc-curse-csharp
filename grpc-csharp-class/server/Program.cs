1adb71a9a488fc8dfa1ac8ca3b0d22a71d021e11
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-16 21:50:24 -0500

    server

--- grpc-csharp-class/server/Program.cs
+++ grpc-csharp-class/server/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Grpc.Core;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +10,31 @@ namespace server
 {
     class Program
     {
+        const int Port = 50051;
         static void Main(string[] args)
         {
+            Server server = null;
+            try
+            {
+                server = new Server()
+                {
+                    Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+                };
+
+                server.Start();
+                Console.WriteLine("The server is listening on the port: " + Port);
+                Console.ReadKey();
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("The server failed to start: " + e.Message);
+                throw;
+            } finally
+            {
+                if (server != null)
+                    server.ShutdownAsync().Wait();
+            }
         }
     }
 }
