c033ca7f486c653ffeaf3af00a56c8cb94e169ea
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-19 00:37:53 -0500

    unary grpc api

--- grpc-csharp-class/client/Program.cs
+++ grpc-csharp-class/client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Dummy;
+using Greet;
 using Grpc.Core;
 using System;
 using System.Collections.Generic;
@@ -20,8 +21,24 @@ namespace client
                     Console.WriteLine("The client connected sussesfully");
             });
 
+            //con dummy.proto
+            //var client = new DummyService.DummyServiceClient(channel);
 
-            var client = new DummyService.DummyServiceClient(channel);
+            //con greeting.proto
+            var client = new GreetingService.GreetingServiceClient(channel);
+
+            var greeting = new Greeting() {
+                FirstName = "Edison",
+                LastName = "Plaza"
+            };
+
+            var request = new GreetingRequest() { Greeting = greeting };
+
+            var response = client.Greet(request);
+
+            Console.WriteLine(response.Result);
+            //-------------------------------------
+
             channel.ShutdownAsync().Wait();
             Console.ReadKey();
 
