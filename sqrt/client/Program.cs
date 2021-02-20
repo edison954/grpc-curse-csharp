8015127f60d2867fd57f7966f465650cfff9f2f4
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-19 23:15:19 -0500

    Error codes

---
+++ sqrt/client/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            const string target = "127.0.0.1:50051";
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected sussesfully");
+            });
+
+
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
