34d76c48efebc27b72222cfb78b7336b307eab07
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-22 23:32:39 -0500

    grpc-blog

---
+++ grpc-blog/client/Program.cs
@@ -0,0 +1,32 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static async Task Main(string[] args)
+        {
+
+
+            Channel channel = new Channel(target, ChannelCredentials.Insecure);
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected sussesfully");
+            });
+
+
+
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
