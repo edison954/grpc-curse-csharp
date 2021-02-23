dec7e0b843fc13aedc29739021bba53a2c37999e
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-23 01:07:03 -0500

    grpc-blog list blog strem server

--- grpc-blog/server/BlogServiceImpl.cs
+++ grpc-blog/server/BlogServiceImpl.cs
@@ -104,5 +104,25 @@ namespace server
 
         }
 
+        public override async Task ListBlog(ListBlogRequest request, IServerStreamWriter<ListBlogResponse> responseStream, ServerCallContext context)
+        {
+            var filter = new FilterDefinitionBuilder<BsonDocument>().Empty;
+            var result = mongoCollection.Find(filter);
+
+            foreach (var item in result.ToList())
+            {
+                await responseStream.WriteAsync(new ListBlogResponse()
+                {
+                    Blog = new Blog.Blog()
+                    {
+                        Id = item.GetValue("_id").ToString(),
+                        AuthorId = item.GetValue("author_id").AsString,
+                        Content = item.GetValue("content").AsString,
+                        Title = item.GetValue("title").AsString
+                    }
+                });
+            }
+        }
+
     }
 }
