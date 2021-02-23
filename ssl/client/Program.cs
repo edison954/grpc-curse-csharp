abed9ec4a5e7a52606cff0bf1d88061022864de0
Edison Arley Plaza Marin <edison954@gmail.com>
2021-02-22 21:39:26 -0500

    ssl

---
+++ ssl/client/Program.cs
@@ -0,0 +1,51 @@
+﻿using Greet;
+using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    class Program
+    {
+        const string target = "127.0.0.1:50051";
+        static async Task Main(string[] args)
+        {
+
+            var clientCert = File.ReadAllText("ssl/client.crt");
+            var clientKey = File.ReadAllText("ssl/client.key");
+            var caCrt = File.ReadAllText("ssl/ca.crt");
+
+            var channelCredentials = new SslCredentials(caCrt, new KeyCertificatePair(clientCert, clientKey));
+
+
+
+            Channel channel = new Channel("localhost",50051, channelCredentials);
+            await channel.ConnectAsync().ContinueWith((task) =>
+            {
+                if (task.Status == TaskStatus.RanToCompletion)
+                    Console.WriteLine("The client connected sussesfully");
+            });
+
+
+            var client = new GreetingService.GreetingServiceClient(channel);
+            var greeting = new Greeting()
+            {
+                FirstName = "Edison",
+                LastName = "Plaza"
+            };
+
+            // Unary
+            var request = new GreetingRequest() { Greeting = greeting };
+            var response = client.Greet(request);
+            Console.WriteLine(response.Result);
+
+
+            channel.ShutdownAsync().Wait();
+            Console.ReadKey();
+        }
+    }
+}
